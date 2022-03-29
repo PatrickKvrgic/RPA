b176dff39fb8ec5f2c1b2e60ba12dac24ba85334
PatrickKvrgic <patrikkvrgic@gmail.com>
2022-03-29 09:19:06 +0200

    ponovitev LINQ

---
+++ VajaBaze/VajaBaze/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VajaBaze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BazaZaVajeEntities context = new BazaZaVajeEntities();
+            //vsi dobavitelji
+            var x1 = from a in context.DOBAVITELJ
+                     select a;
+            //var x1 = context.DOBAVITELJ.Select(e => e); //isto
+            foreach(var y in x1)
+            {
+                Console.WriteLine(y.D_IME + " " + y.D_KONTAKT);
+            }
+            Console.ReadLine();
+        }
+    }
+}
